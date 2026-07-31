3e4448d038236b3dc3dd30ef6d8c514c26b6ec09
tslacc <46695088+tslacc@users.noreply.github.com>
2026-07-30 21:14:56 -0400

    Completed evaluation
    Supports basic +-*/ and (). No exponentials or functions yet

--- expr_eval.c
+++ expr_eval.c
@@ -5,10 +5,15 @@ struct Part{
 	float value;
 	struct Part *next;	
 };
-
+static int left_associative_operator(char c){
+	if(c=='^') return 0;
+	if(c=='*'||c=='/') return 1;
+	if(c=='+'||c=='-') return 1;
+	return 0;
+}
 static int operator_precedence(char c){
 	if(c=='^') return 4;
-	if(c=='x'||c=='/') return 3;
+	if(c=='*'||c=='/') return 3;
 	if(c=='+'||c=='-') return 2;
 	return 0;
 }
@@ -18,14 +23,6 @@ static int char_validation(char c){
 	if(c == '+' || c == '-' || c == '*' || c == '/' || c=='^') return 2;
 	if(c == '(' || c == ')') return 2;
 	return 0;
-}
-
-static void cleanup(struct Part *p){
-	if(p->next!=NULL){
-		cleanup(p->next);
-	}
-	free(p);
-	return;
 }
 
 //Strip the buffer of spaces and non valid chars
@@ -56,7 +53,7 @@ static struct Part *buffer_to_parts(char *input){
 					if(*input=='.') {
 						periodSeen++;
 						if(periodSeen>1) {
-							printf("Syntax error");
+							//printf("Syntax error");
 							return 0;
 						}
 					}
@@ -86,13 +83,11 @@ static struct Part *buffer_to_parts(char *input){
 				input++;
 				break;
 			default: //error
-				cleanup(unread_head);
 				return NULL;
 		}
 	}
 	return unread_head->next;
 }
-
 //Shunting yard algorithm
 //https://en.wikipedia.org/wiki/Shunting_yard_algorithm#The_algorithm_in_detail
 float eval_expr(char *input){
@@ -111,63 +106,150 @@ float eval_expr(char *input){
 	struct Part *out_queue_back = NULL;
 	
 	while(work!=NULL){
+		printf("Process unit %u, (%u, %c), %f\n", work, work->operator, work->operator, work->value);
 		if(work->operator==0){
-			printf("Number %f to output queue\n", work->value);
-			if(out_queue_front==NULL){
-				out_queue_front = work;
-				out_queue_back = work;
+			printf("\tNumber %f to output queue\n", work->value);
+			if(out_queue_front == NULL){
+				out_queue_front = out_queue_back = work;
 			} else {
 				out_queue_back->next = work;
+				out_queue_back = work;
 			}
-			work = work->next;
+			work=work->next;
 		} else if(work->operator=='('){ //push onto operator stack
-			struct Part *tmp = work;
-			work = work->next;
-			tmp->next = op_stack;
-			op_stack = tmp;
+			printf("\tLeft paren to op stack\n", op_stack->operator);
+			if(op_stack==NULL){
+				op_stack = work;
+				work=work->next;
+			} else {
+				struct Part *tmp = work;
+				work = work->next;
+				tmp->next = op_stack;
+				op_stack = tmp;
+			}
 		} else if(work->operator==')'){ //Move everything from operator stack to output queue until you find a left paren
+			printf("\tRight paren, move all from op stack to out queue until left paren found\n");
 			while(op_stack != NULL && op_stack->operator!='('){
+				printf("Push %c to op stack\n", op_stack->operator);
 				if(out_queue_front==NULL){
 					out_queue_front = op_stack;
 					out_queue_back = op_stack;
 				} else {
 					out_queue_back->next = op_stack;
+					out_queue_back = op_stack;
 				}
 				op_stack = op_stack->next;
 			}
 			if(op_stack!=NULL && op_stack->operator == '('){
+				printf("\tDiscard left paren\n");
 				op_stack = op_stack->next;
 			} else {
 				//TODO Handle critical input error mismatched parens
 				break;
 			}
 		} else {//This is some kind of operator called o1
-			printf("Operator %c, perform operation\n", work->operator);
+			printf("\tOperator %c to op stack\n", work->operator);
 			//while (
 				//there is an operator o2 at the top of the operator stack which is not a left parenthesis
 				//and (o2 has greater precedence than o1 
 				//or (o1 and o2 have the same precedence and o1 is left-associative))
-			while(op_stack!=NULL && op_stack->operator != '(' 
-					&& operator_precedence(op_stack->operator) > operator_precedence(work->operator)) {
-					// pop o2 from the operator stack into the output queue
-					if(out_queue_front==NULL){
-						out_queue_front = op_stack;
-						out_queue_back = op_stack;
-					} else {
-						out_queue_back->next = op_stack;
-					}
-					op_stack = op_stack->next;
-					//push o1 onto the operator stack
-					struct Part *tmp = work;
-					work = work->next;
-					tmp->next = op_stack;
-					op_stack = tmp;
+			while(
+				op_stack!=NULL && op_stack->operator != '(' 
+					&& (
+						operator_precedence(op_stack->operator) > operator_precedence(work->operator)
+						|| (
+							operator_precedence(op_stack->operator) == operator_precedence(work->operator)
+							&& left_associative_operator(work->operator)
+						)
+					) 
+				){
+				printf("\tPrecedence of o1 o2, %c %c\n", work->operator, op_stack->operator);
+
+				// pop o2 from the operator stack into the output queue
+				if(out_queue_front==NULL){
+					out_queue_front = op_stack;
+					out_queue_back = op_stack;
+				} else {
+					out_queue_back->next = op_stack;
+					out_queue_back = op_stack;
+				}
+				op_stack = op_stack->next;		
 			}
-			
+			//push o1 onto the operator stack
+			printf("\tPushing o1 %c to op stack\n", work->operator);
+			struct Part *tmp = work;
+			work = work->next;
+			tmp->next = op_stack;
+			op_stack = tmp;
 		}
 	}
-	printf("\n");
-	//Cleanup all tokens in the final list
-	cleanup(out_queue_front);
-	return 0;
+
+	while(op_stack!=NULL){
+		//printf("Op stack contains %u (%u, %c), %f\n", op_stack, op_stack->operator, op_stack->operator, op_stack->value);
+		if(op_stack->operator=='(') {//CRITICAL ERROR
+			break;
+		}
+		if(out_queue_front==NULL){
+			out_queue_front = op_stack;
+			out_queue_back = op_stack;
+		} else {
+			out_queue_back->next = op_stack;
+			out_queue_back = op_stack;
+		}
+		if(op_stack->next == NULL){
+			break;
+		}
+		op_stack = op_stack->next;
+	}
+	//printf("Done making rpn\n");
+	//printf("%u, %u\n", out_queue_front, out_queue_back);
+	struct Part *placeholder = out_queue_front;
+	while(out_queue_front!=NULL){
+		printf("Object %u contains (%u,%c) %f \n", out_queue_front, out_queue_front->operator, out_queue_front->operator, out_queue_front->value);
+		if(out_queue_front->next == NULL) break;
+		out_queue_front = out_queue_front->next;
+	}
+	out_queue_front = placeholder;
+	//printf("build eval stack\n");
+	struct Part *eval_stack = out_queue_front;
+	//printf("Add %f to eval stack\n", out_queue_front->value);
+	out_queue_front = out_queue_front->next;
+	while(out_queue_front!=NULL){
+		//printf("Checking unit %u (%u,%c) %f\n", out_queue_front, out_queue_front->operator, out_queue_front->operator, out_queue_front->value);
+		if(out_queue_front->operator == 0) {
+			//printf("Add %f to eval stack\n", out_queue_front->value);
+			struct Part *tmp = out_queue_front;
+			out_queue_front = out_queue_front->next;
+			tmp->next = eval_stack;
+			eval_stack = tmp;
+		} else {
+			struct Part *op1 = eval_stack;
+			eval_stack = eval_stack->next;
+			//printf("Evaluating operator %c in %u %u, v %f %f\n", out_queue_front->operator, op1, eval_stack, op1->value, eval_stack->value);
+			switch (out_queue_front->operator) {
+				case '+':
+					eval_stack->value = eval_stack->value + op1->value;
+					break;
+				case '-':
+					eval_stack->value = eval_stack->value - op1->value;
+					break;
+				case '*':
+					eval_stack->value = eval_stack->value * op1->value;
+					break;
+				case '/':
+					eval_stack->value = eval_stack->value / op1->value;
+					break;
+			}
+			free(op1);
+			struct Part *tmp = out_queue_front;
+			out_queue_front = out_queue_front->next;
+			free(tmp);
+		}
+		
+	}
+
+	float result = eval_stack->value;
+	//printf("r = %f, free eval stack %u\n", result, eval_stack);
+	free(eval_stack);
+	return result;
 }
